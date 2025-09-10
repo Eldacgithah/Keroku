9199a9b7214d2399797bad6fedf8ed27ac2676f5
Eldacgithah <eeeldac@gmail.com>
2025-09-10 14:45:33 +0300

    Update Startapi.py

--- Startapi.py
+++ Startapi.py
@@ -2,15 +2,23 @@ import subprocess
 import time
 import os
 import sys
-import signal
+import threading
 
 proc = subprocess.Popen(
     ["python3", "-m", "heroku", "--no-web"],
     stdin=subprocess.PIPE,
     stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    text=True
+    stderr=subprocess.STDOUT,
+    text=True,
+    bufsize=1
 )
+
+def read_output(p):
+    for line in p.stdout:
+        print(line, end="")
+
+t = threading.Thread(target=read_output, args=(proc,))
+t.start()
 
 time.sleep(10)
 proc.stdin.write("N\n")
@@ -23,18 +31,10 @@ proc.stdin.write("7d3ea0c0d4725498789bd51a9ee02421\n")
 proc.stdin.flush()
 
 proc.wait()
+t.join()
 
 script_path = os.path.abspath(__file__)
-pid = os.getpid()
-
-def remove_self():
-    try:
-        os.remove(script_path)
-    except:
-        pass
-
-import threading
-t = threading.Thread(target=remove_self)
-t.start()
-
-os.kill(pid, signal.SIGTERM)
+try:
+    os.remove(script_path)
+except:
+    pass
